ccd67785ebcf91d9f42f6105113de9e9f9d107f9
chaight <chaight@wisc.edu>
2024-05-14 10:20:17 -0500

    fixed bugs in geo_main.py

--- geo_main.py
+++ geo_main.py
@@ -25,16 +25,6 @@ csv_households_file_path = "data/homedata.csv"
 households = pd.read_csv(csv_households_file_path)
 stores = pd.read_csv(csv_stores_file_path)
 
-
-#number of steps
-num_steps = 5
-#intialize model with stores and household data in a pandas df
-model = GeoModel(stores,households)
-#Run model for given number of iterations
-for i in range(num_steps):
-    model.step()
-
-
 class GeoModel(mesa.Model):
 
     #intialize all agents
@@ -44,26 +34,26 @@ class GeoModel(mesa.Model):
         self.schedule = mesa.time.RandomActivation(self)
 
         # Create store agents and add to model
-        for row,index in stores.iterrows():
-            agent = Store(index, row["category"],row["latitude"],row["longitude"])
+        for index,row in stores.iterrows():
+            agent = Store(index+len(households), self, row["category"],row["latitude"],row["longitude"],row["FSA"])
             self.space.add_agents(agent)
 
         #Create household agents and add to model
-        for row,index in households.itterows():
+        for index,row in households.iterrows():
             if row["category"] == "ERHC":
-                agent = Household(index,"ERHC",row["latitude"],row["longitude"],row["mfai"],ERHCMAX,ERHCFARTHERPROB,CLOSERPROB)
+                agent = Household(index,self, "ERHC",row["latitude"],row["longitude"],0,ERHCMAX,ERHCFARTHERPROB,CLOSERPROB)
                 self.schedule.add(agent)
                 self.space.add_agents(agent)
             if row["category"] == "ERLC":
-                agent = Household(index,"ERLC",row["latitude"],row["longitude"],row["mfai"],ERLCMAX,ERLCFARTHERPROB,CLOSERPROB)
+                agent = Household(index,self, "ERLC",row["latitude"],row["longitude"],0,ERLCMAX,ERLCFARTHERPROB,CLOSERPROB)
                 self.schedule.add(agent)
                 self.space.add_agents(agent)
             if row["category"] == "LRHC":
-                agent = Household(index,"LRHC",row["latitude"],row["longitude"],row["mfai"],LRHCMAX,LRHCFARTHERPROB,CLOSERPROB)
+                agent = Household(index,self, "LRHC",row["latitude"],row["longitude"],0,LRHCMAX,LRHCFARTHERPROB,CLOSERPROB)
                 self.schedule.add(agent)
                 self.space.add_agents(agent)
             if row["category"] == "LRLC":
-                agent = Household(index,"LRLC",row["latitude"],row["longitude"],row["mfai"],LRLCMAX,LRLCFARTHERPROB,CLOSERPROB)
+                agent = Household(index,self, "LRLC",row["latitude"],row["longitude"],0,LRLCMAX,LRLCFARTHERPROB,CLOSERPROB)
                 self.schedule.add(agent)
                 self.space.add_agents(agent)
   
@@ -86,9 +76,9 @@ class Store(mesa_geo.GeoAgent):
 
     """
 
-    def __init__(self: "Store", store_id: int, category: str, lat: float, lon: float, fsa: int) -> None:
+    def __init__(self: "Store",  model, store_id: int, category: str, lat: float, lon: float, fsa: int) -> None:
         """Initialize food store."""
-        super().__init__(self, store_id,model,Point(lat,lon),"epsg:3857") # epsg:3857 is the mercator projection
+        super().__init__(store_id,model,Point(lat,lon),"epsg:3857") # epsg:3857 is the mercator projection
         self.category = str(category)
         self.fsa = int(fsa)
 
@@ -105,9 +95,9 @@ class Household(mesa_geo.GeoAgent):
 
     """
 
-    def __init__(self, house_id, household_type, lat, lon, mfai, mfai_max: int, farther_prob: float, closer_prob: float):
+    def __init__(self, house_id,model,  household_type, lat, lon, mfai, mfai_max: int, farther_prob: float, closer_prob: float):
         """Initialize agent's attributes."""
-        super().__init__(self,house_id,model,Point(lat,lon),"epsg:2022")
+        super().__init__(house_id,model,Point(lat,lon),"epsg:2022")
         self.household_type = str(household_type)
         self.mfai = int(mfai)
         self.mfai_max = mfai_max
@@ -123,24 +113,30 @@ class Household(mesa_geo.GeoAgent):
         """
         #(1) find all stores within SEARCHRADIUS
         #TODO using geo-mesa
-
+        closest_stores = model.space.get_neighbors_within_distance(self,SEARCHRADIUS)
         #init variables to easier names
         mfai_max = self.mfai_max
         farther_prob = self.farther_prob
         closer_prob = self.closer_prob
-
-        #(2) choose one from list based on probs
-        spm_choice = nearest_spms[0]  #tuple of store and distance, closest SPM
-        cspm_choice = nearest_cspms[0]  #tuple of store and distance, closest CSPM
+        """
         if(spm_choice[1]>cspm_choice[1]):
             #if farther, choose SPM with fartherprob
             chosen = random.choices([spm_choice[0],cspm_choice[0]], weights=[farther_prob,1-farther_prob])[0]
         else:
             #if closer, choose SPM with closerprob
             chosen = random.choices([spm_choice[0],cspm_choice[0]], weights=[closer_prob,1-closer_prob])[0]
+        """
         #(3) calculate mfai like self.mfai = chosenMarket.fsa / mfai_max
-        food_from_visit =  int ((chosen.fsa / mfai_max)*100) #increment household's mfai by store's score
-        self.mfai += food_from_visit
+        #food_from_visit =  int ((chosen.fsa / mfai_max)*100) #increment household's mfai by store's score
+        #self.mfai += food_from_visit
         #TODO implement behavioral rules from slide 10 of ABM literature presentation: 0.8 for LC, 0.8 for LR, movement speeds?
             # foodFromVisit =  chosen.fsa * hasNoCar*0.8 *hasLowResource*0.8  #increment household's mfai by store's score
             # self.mfai += int ((foodFromVisit / mfai_max)*100)
+
+#number of steps
+num_steps = 5
+#intialize model with stores and household data in a pandas df
+model = GeoModel(stores,households)
+#Run model for given number of iterations
+for i in range(num_steps):
+    model.step()
