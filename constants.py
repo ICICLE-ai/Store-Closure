f9596241333686f8a3ff425ad6b5985da7d3e22d
chaight <chaight@wisc.edu>
2024-07-01 10:04:55 -0500

    more work on streamling creation of agents and making changing location easier

--- constants.py
+++ constants.py
@@ -4,11 +4,6 @@ SEARCHRADIUS = 500
 #crs geometry for map (this is web mercator)
 CRS = "3857"
 
-#Census codes
-FIPSCODE = "39049" #fips code for franklin county ohio - use this to fill in state code and county code below
-COUNTYCODE = "049" #code for franklin county
-STATECODE = "39" #code for Ohio
-
 #File paths
 HOUSEHOLDSFILEPATH = "data/households.csv"
 STORESFILEPATH = "data/stores.csv"
---
+++ data/create_households.py
@@ -0,0 +1,2 @@
+#FIPS code for a county. Used to specify location. Change this code and run this file to create data for a different county.
+FIPSCODE = "39049" #default set to franklin county Ohio
